1bb4a962720c9e33427b97a385b7004c863bd51d
lee-dias <leedias.dev@gmail.com>
2024-03-07 15:17:10 +0000

    Update HeroPerks gives all messages

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -15,28 +15,48 @@ namespace HeroPerks
         static void Main(string[] args)
         {
 
-            Perks PlayerPerks = 0;
+            Perks HeroPerks = 0;
         foreach (char c in args[0])
         {
             switch (c)
             {
                 case 'w':
-                    PlayerPerks ^= Perks.WaterBreathing;
+                    HeroPerks ^= Perks.WaterBreathing;
                     break;
                 case 'a':
-                    PlayerPerks ^= Perks.AutoHeal;
+                    HeroPerks ^= Perks.AutoHeal;
                     break;
                 case 's':
-                    PlayerPerks ^= Perks.Stealth;
+                    HeroPerks ^= Perks.Stealth;
                     break;
                 case 'd':
-                    PlayerPerks ^= Perks.DoubleJump;
+                    HeroPerks ^= Perks.DoubleJump;
                     break;
                 default:
                     Console.WriteLine("Unknown perk!");
                     return;
             }
         }
+
+        if (HeroPerks == 0)
+        {
+            Console.WriteLine("No perks at all!");
+        }else{
+            Console.WriteLine($"Player perks: {HeroPerks}");
+        }
+
+
+
+        if ((HeroPerks & Perks.Stealth) == Perks.Stealth && 
+        (HeroPerks & Perks.DoubleJump) == Perks.DoubleJump)
+        {
+            Console.WriteLine("Silent jumper!");
+        }
+
+        if ((HeroPerks & Perks.AutoHeal) != Perks.AutoHeal)
+        {
+            Console.WriteLine("Not gonna make it!");
+        }
+        }
     }
-}
 }
