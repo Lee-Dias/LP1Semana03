bfa2da45520d56001160be44e0db1ee7d3876c85
lee-dias <leedias.dev@gmail.com>
2024-03-07 14:12:30 +0000

    add RightSize all working

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string Str in args){
+
+                if(Str.Length >3 & Str.Length <6){
+                    Console.WriteLine(Str);
+                }
+                if(Str.Length>6){
+                    return;
+                }
+
+            }
+
+        }
+    }
+}
+
