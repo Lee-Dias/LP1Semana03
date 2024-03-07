c512e1c381de4de68a7a6c79d7853041626c6c5d
lee-dias <leedias.dev@gmail.com>
2024-03-07 14:20:35 +0000

    add ChangeString and asks for a string and a character

---
+++ ChangeString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tell me something");
+            string str = Console.ReadLine();
+            Console.WriteLine("give me one character");
+            string car = Console.ReadLine();
+
+    
+
+        }
+    }
+}
