32b34baac63100078af801bc096fe47d129917be
lee-dias <leedias.dev@gmail.com>
2024-03-07 15:06:34 +0000

    Update HeroPerks has enum with all 4 perks

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -2,10 +2,20 @@
 
 namespace HeroPerks
 {
-    class Program
+    [Flags]
+    enum Perks
     {
+        WaterBreathing = 1 << 0,
+        Stealth = 1 << 1,
+        AutoHeal = 1 << 2,
+        DoubleJump = 1 << 3
+    }
+    class Program
+    {   
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("Hello, World!");
         }
     }
