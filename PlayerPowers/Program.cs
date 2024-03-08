179294abee0e2205e01ef4250493cd9577b707f8
lee-dias <leedias.dev@gmail.com>
2024-03-08 13:52:28 +0000

    Update PlayerPowers asks for how many players and asks which powers they want

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -6,7 +6,19 @@ namespace PlayerPowers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Powers PlayerPowers = 0;
+            Console.WriteLine("How many players will play?");
+            string jogadores = Console.ReadLine();
+            int n = int.Parse(jogadores);
+            int[] players = new int[n];
+            foreach (int p in players ){
+                Console.WriteLine("What are your powers? u can combine!!");
+                Console.WriteLine("Type F for Fly");
+                Console.WriteLine("Type X for XRayVision");
+                Console.WriteLine("Type S for SuperStrength");
+                string powers = Console.ReadLine();
+
+            }
         }
     }
 }
